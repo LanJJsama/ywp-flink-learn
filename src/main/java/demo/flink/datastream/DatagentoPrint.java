34e591522725f27ad95830c984aae231af36ec6e
lanji <lanjiaojiao93@gmail.com>
2024-11-03 22:11:44 +0800

    增加文本数据源到Print

--- src/main/java/demo/flink/datastream/DatagentoPrint.java
+++ src/main/java/demo/flink/datastream/DatagentoPrint.java
@@ -10,7 +10,7 @@ public class DatagentoPrint {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         // 设置文本数据源
-        DataStream<String> text = env.readTextFile("G:\\GitHub\\github-recovery-codes.txt");
+        DataStream<String> text = env.readTextFile("G:\\Test\\新建文本文档.txt");
         // 打印内容到控制台
         text.print();
         env.execute("Window WordCount");
